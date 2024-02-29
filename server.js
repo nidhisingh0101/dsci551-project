b2aca4c6b56350d4c35971fd08183c67bcfee790
nidhisingh0101 <46805705+nidhisingh0101@users.noreply.github.com>
2024-02-29 13:12:27 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express =require('express')
-const articleRouter = require("./routes/articles")
-const mongoose  = require('mongoose')
-const app = express()
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
-app.set('view engine', 'ejs')
-app.get('/',(req,res)=>(
-    res.render('index')
-))
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.use('/articles', articleRouter)
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/healthcare', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
-app.listen(3000)
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
